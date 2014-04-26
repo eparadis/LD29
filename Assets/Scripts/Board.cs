5725f122cc9a16f1c3ad0215f0b96da777a87294
Ed Paradis <legomaniac@gmail.com>
2014-04-26 09:00:31 -0700

    create board from data using a few different tile types

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,6 +4,21 @@ using System.Collections;
 public class Board : MonoBehaviour {
 
 	public GameObject waterTilePrefab;
+	public GameObject landTilePrefab;
+	public GameObject mineTilePrefab;
+
+	private string[] levelData = {
+	//   0123456789
+		"0000000000",	// 0
+		"0011002000",	// 1
+		"0011000000",	// 2
+		"0000002000",	// 3
+		"0000000000",	// 4
+		"0020000000",	// 5
+		"0000000001",	// 6
+		"0000200011",	// 7
+		"0000001111",	// 8
+		"0000111111"};	// 9
 
 	// Use this for initialization
 	void Start () {
@@ -25,10 +40,23 @@ public class Board : MonoBehaviour {
 			for( int col = 0; col < numCols; col += 1)
 			{
 				Vector3 pos = new Vector3( row, col, 0);
-				GameObject tile = (GameObject) GameObject.Instantiate( waterTilePrefab);
+				GameObject tile;
+				switch( levelData[row][col])
+				{
+				default:
+				case '0':
+					tile = (GameObject) GameObject.Instantiate( waterTilePrefab);
+					break;
+				case '1':
+					tile = (GameObject) GameObject.Instantiate( landTilePrefab);
+					break;
+				case '2':
+					tile = (GameObject) GameObject.Instantiate( mineTilePrefab);
+					break;
+				}
 				tile.transform.parent = gameObject.transform;
 				tile.transform.localPosition = pos;
-				tile.name = waterTilePrefab.name + string.Format ("_r{0}_c{1}", row, col);
+				tile.name = string.Format ("board_tile_r{0}_c{1}", row, col);
 			}
 		}
 	}
